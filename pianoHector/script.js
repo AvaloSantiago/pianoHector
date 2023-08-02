5bdcf9b713389121fc3078ef53b3a637bf68825d
AvaloSantiago <112868689+AvaloSantiago@users.noreply.github.com>
2023-08-02 20:06:24 -0300

    suena el piano

--- pianoHector/script.js
+++ pianoHector/script.js
@@ -1,17 +1,26 @@
-
 const synth = new Tone.Synth().toDestination();
-const now = Tone.now()
+const now = Tone.now();
+
 document.addEventListener("DOMContentLoaded", () => {
-  let sonidob = ['C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3']
-  const doblanca = document.querySelector(".do");
-  const blancas = document.querySelector(".t")
-  for (let i = 0; i < blancas.length; i++) {
-    const element = array[i];
-    blancas[i].addEventListener("click", async () => {
-      console.log("jaj")
-      synth.triggerAttackRelease("sonidob[i]", "8n");
-    })
+  let sonidob = ['C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3', 'C4', 'D4', 'F4', 'G4', 'A4'];
+
+  const blancas = document.querySelectorAll(".blanca");
+  const negras = document.querySelectorAll(".containernegras button");
+
+  // Función para reproducir una nota
+  function reproducirNota(nota) {
+    synth.triggerAttackRelease(nota, "8n");
   }
-  doblanca.addEventListener("click", async () => {
+
+  blancas.forEach((boton, index) => {
+    boton.addEventListener("click", () => {
+      reproducirNota(sonidob[index]);
+    });
+  });
+
+  negras.forEach((boton, index) => {
+    boton.addEventListener("click", () => {
+      reproducirNota(sonidob[index + 7]); // Los botones negros están después de las notas blancas en el array
+    });
   });
 });
